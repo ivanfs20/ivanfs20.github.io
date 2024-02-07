e4ae8fec2da1f17cd75b7c98a596eaf2f93c001c
Carlos Ivan Flores Sanchez <crascifs@gmail.com>
2024-02-06 18:54:21 -0600

    Prueba Post php

---
+++ php/Gestor.php
@@ -0,0 +1,10 @@
+<?php
+    $Nombre=$_POST['Nombre'];
+    $Correo=$_POST['Correo'];
+    $Asunto=$_POST['Asunto'];
+
+    echo "El nombre es: ".$Nombre."<br>";
+    echo "El correo es: ".$Correo."<br>";
+    echo "El asunto es: ".$Asunto."<br>";
+    
+?>
